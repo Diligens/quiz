efca248672df7629d521db0fc1d9ed226ad14081
Artem Sharyukov <artem.sharyukov@emergn.com>
2019-09-03 13:31:24 +0300

    events: done

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Quiz.css';
 import './bootstrap.min.css';
+import PropTypes from 'prop-types/prop-types';
 
 function Hero() {
-    return(
+    return (
         <div className="jumbotron col-10 offset-1">
             <h1>Quiz</h1>
             <p>Select the book writter by the author shown</p>
@@ -11,52 +12,73 @@ function Hero() {
     )
 }
 
-function Book({title}) {
+function Book({ title, onClick }) {
     return (
-        <div className="answer">
+        <div className="answer" onClick={() => { onClick(title); }}>
             <h4>{title}</h4>
         </div>
     )
 }
 
-function Turn({author, books}) {
-    return(
-        <div className="row turn" style={{backgroundColor: "white"}}>
+function Turn({ author, books, highlight, onAnswerSelected}) {
+    function highlightToBgColor(highlight) {
+        const mapping = {
+            'none': '',
+            'correct': 'green',
+            'wrong': 'red'
+        }
+        return mapping[highlight];
+    }
+
+    return (
+        <div className="row turn" style={{ backgroundColor: highlightToBgColor(highlight) }}>
             <div className="col-4 offset-1">
-                <img src={author.imageUrl} className="authorimage" alt="Author"/>
+                <img src={author.imageUrl} className="authorimage" alt="Author" />
             </div>
             <div className="col-6">
-                {books.map((title) => <Book title={title} key={title}/>)}
+                {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected}/>)}
             </div>
         </div>
     )
 }
 
+Turn.propTypes = {
+    author: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imageUrl: PropTypes.string.isRequired,
+        imageSource: PropTypes.string.isRequired,
+        // books: PropTypes.arrayOf(PropTypes.string).isRequired
+    }),
+    // books: PropTypes.array(PropTypes.string).isRequired, // errors, I think, need to use another type check tool https://github.com/facebook/prop-types/blob/master/README.md#difference-from-reactproptypes-dont-call-validator-functions
+    highlight: PropTypes.string.isRequired,
+    onAnswerSelected: PropTypes.func.isRequired
+}
+
 function Continue() {
-    return(
+    return (
         <div>
-            
+
         </div>
     )
 }
 
 function Footer() {
-    return(
+    return (
         <footer id="footer" className="row">
-           Footer
+            Footer
         </footer>
     )
 }
 
-function Quiz({turnData}) {
-  return (
-    <div className="container-fluid">
-        <Hero/>
-        <Turn {...turnData}/>
-        <Continue/>
-        <Footer/>
-    </div> 
-  );
+function Quiz({ turnData, highlight, onAnswerSelected}) {
+    return (
+        <div className="container-fluid">
+            <Hero />
+            <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+            <Continue />
+            <Footer />
+        </div>
+    );
 }
 
 export default Quiz;
--- src/Quiz.test.js
+++ src/Quiz.test.js
@@ -1,12 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Quiz from './Quiz';
+import Enzyme, { mount, shallow, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+const state = {
+    turnData: {
+        books: ['The Shining', 'IT', 'David Copperfield', 'A Tale of Two Cities'],
+        author: {
+            name: 'Charles Dickens',
+            imageUrl: 'images/authors/charlesdickens.jpg',
+            imageSource: 'Wikimedia Commons',
+            books: ['David Cooperfield', 'A Tale of Two Cities']
+        },
+    },
+    highlight: 'none'
+}
+
 
 describe("Quiz", () => {
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<Quiz />, div);
+        ReactDOM.render(<Quiz {...state} onAnswerSelected={ () => {} }/>, div);
         ReactDOM.unmountComponentAtNode(div);
+    });
+
+    describe("When no answer has been selected", () => {
+        let wrapper;
+        beforeAll(()=> {
+            wrapper = mount(<Quiz {...state} onAnswerSelected={ () => {}}/>)
+        })
+
+        it("should have no background color", () => {
+            expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('');
+        })
+    });
+
+    describe("When the wrong answer has been selected", () => {
+        let wrapper;
+        beforeAll(()=> {
+            wrapper = mount(<Quiz {...(Object.assign({}, state, {highlight: 'wrong'}))} onAnswerSelected={ () => {}}/>)
+        })
+
+        it("should have a red background color", () => {
+            expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('red');
+        })
+    });
+
+    describe("When the correct answer has been selected", () => {
+        let wrapper;
+        beforeAll(()=> {
+            wrapper = mount(<Quiz {...(Object.assign({}, state, {highlight: 'correct'}))} onAnswerSelected={ () => {}}/>)
+        })
+
+        it("should have a red background color", () => {
+            expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('green');
+        })
+    });
+
+    describe("When the first answer is selected", () => {
+        let wrapper;
+        const handleAnswerSelected = jest.fn();
+
+        beforeAll(()=> {
+            wrapper = mount(<Quiz {...state} onAnswerSelected={ handleAnswerSelected }/>);
+            wrapper.find('.answer').first().simulate('click');
+        });
+
+        it("onAnswerSelected should be called", () => {
+            expect(handleAnswerSelected).toHaveBeenCalled();
+        });
+
+        it("should receive The Shining", () => {
+            expect(handleAnswerSelected).toHaveBeenCalledWith("The Shining");
+        });
     });
 })
 
